f16a0eb41fcc48cd519e048ecfbc950c62ab5653
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2021-06-15 13:27:08 +0300

    Update Henkilotiedot.java

--- KantaCDA-API/src/main/java/fi/kela/kanta/interfaces/Henkilotiedot.java
+++ KantaCDA-API/src/main/java/fi/kela/kanta/interfaces/Henkilotiedot.java
@@ -25,22 +25,7 @@ import java.util.List;
  */
 public interface Henkilotiedot extends Serializable {
 
-    /**
-     * Nimilista henkilölle määrittellyistä nimistä. Ensimmäisenä palautettava nimi on oletuksena käytetty kutsumanimi.
-     *
-     * @return Lista (etu)nimistä.
-     */
-    public List<String> getEtunimet();
-
-    /**
-     * @return Henkilön sukunimi.
-     */
-    public String getSukunimi();
-
-    /**
-     * @return Henkilötunnus.
-     */
-    public String getHetu();
+   
 
     /**
      * @return Syntymäaika.
