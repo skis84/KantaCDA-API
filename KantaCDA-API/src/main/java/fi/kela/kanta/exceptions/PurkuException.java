167b1a21de86710ef05529b099227a606b468b08
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2021-06-16 15:30:52 +0300

    Update PurkuException.java

--- KantaCDA-API/src/main/java/fi/kela/kanta/exceptions/PurkuException.java
+++ KantaCDA-API/src/main/java/fi/kela/kanta/exceptions/PurkuException.java
@@ -38,4 +38,30 @@ public class PurkuException extends Exception {
     public PurkuException(Throwable cause) {
         super(cause);
     }
+  
+  
+  
+    private static final long serialVersionUID = 412351L;
+
+    public PurkuException() {
+        super();
+    }
+
+    public PurkuException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public PurkuException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PurkuException(String message) {
+        super(message);
+    }
+
+    public PurkuException(Throwable cause) {
+        super(cause);
+    }
+  
+  
 }
