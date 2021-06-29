3d2a06f8c08594039f9bae9a3602bca1dfbd043a
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2021-06-29 10:55:57 +0300

    Update MissingAttributeException.java

--- KantaCDA-API/src/main/java/fi/kela/kanta/exceptions/MissingAttributeException.java
+++ KantaCDA-API/src/main/java/fi/kela/kanta/exceptions/MissingAttributeException.java
@@ -5,25 +5,7 @@ import java.text.MessageFormat;
 
 public class MissingAttributeException extends Exception {
 
-    private static final long serialVersionUID = 1L;
-    private static final String backupPattern = "Sisäinen attribuutti ''{0}'' puuttuu.";
-    private final String basicMsg;
-
-    public MissingAttributeException(String attributeName) {
-        basicMsg = buildMessage(MissingAttributeException.backupPattern, attributeName);
-    }
-
-    @Override
-    public String getMessage() {
-
-        return basicMsg;
-    }
-
-    @Override
-    public String getLocalizedMessage() {
-
-        return basicMsg;
-    }
+    
 
     private String buildMessage(String pattern, String attributeName) {
         MessageFormat mf = new MessageFormat(pattern);
