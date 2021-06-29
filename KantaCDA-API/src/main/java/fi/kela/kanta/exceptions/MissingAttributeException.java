018a74f9e5cbe32795ae70b69667c6f34cfa17b6
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2021-06-29 11:43:47 +0300

    Update MissingAttributeException.java

--- KantaCDA-API/src/main/java/fi/kela/kanta/exceptions/MissingAttributeException.java
+++ KantaCDA-API/src/main/java/fi/kela/kanta/exceptions/MissingAttributeException.java
@@ -7,9 +7,3 @@ public class MissingAttributeException extends Exception {
 
     
 
-    private String buildMessage(String pattern, String attributeName) {
-        MessageFormat mf = new MessageFormat(pattern);
-        Object[] obj = { attributeName };
-        return mf.format(obj);
-    }
-}
